f733df2f34415253b4ae8bc4adc233942201a950
yhbarve <yhbarve@uwaterloo.ca>
2025-06-12 19:21:30 -0400

    minor: cleaned imports

--- client/src/components/ChatPane.tsx
+++ client/src/components/ChatPane.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import type { Socket } from 'socket.io-client';
 
 interface ChatMessage {
